8d4ca21ca2630aef46485728c95157d3104b4e68
wangwei <wangwei3311@100.me>
2020-09-23 16:06:32 +0800

    1

--- src/view/firstPage/index.vue
+++ src/view/firstPage/index.vue
@@ -1,37 +1,11 @@
 <template>
-    <div>
-        <button @click="bindAdd">add</button>
-    </div>
+  <div class="row">
+  </div>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {}
-    },
-    created() {
-        // this.$toast('asdasd', 1000)
-    },
-    methods: {
-        bindAdd() {
-            // this.$store.commit('increment', {amount: 2})
-            // this.$store.dispatch('a/actionA', {amount: 2}).then(res => {
-            //     console.log(2222)
-            // })
-            this.$toast('asdasd', 1000)
-        }
-    },
-    computed: {
-        count() {
-            return this.$store.state.a.count
-        }
-    },
-    components: {
-    }
-}
+  created() {
+  }
+};
 </script>
-
-<style lang='scss' scoped>
-
-</style>
